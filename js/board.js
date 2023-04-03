cd8fe7fe06f0246d1ea4e484b3fd6c967b07f445
JoshuaLing <joshualing81@gmail.com>
2023-04-03 18:56:39 +1000

    added two player gameplay

--- js/board.js
+++ js/board.js
@@ -3,26 +3,35 @@
 // need to know array index is empty, cross or circle
 //if array value   0 = empty, 1 = cross, 2 = circle
 // function to check iwnner 
-
 const board = {
     0: [0,0,0],
     1: [0,0,0],
     2: [0,0,0],
 
+    gameOver: false,
+    tilesPlayed: 0,
+    winner: 0,
+
     select: function (row, column, player) {
         if (this[row][column] === 0) {
             this[row][column] = player;
         }
+        this.tilesPlayed += 1;
+        this.checkWinner(player);
     },
 
     restart: function () {
         this[0] = [0,0,0];
         this[1] = [0,0,0];
         this[2] = [0,0,0];
+
+        this.winner = 0;
+        this.gameOver = false;
+        this.tilesPlayed = 0;
         //can loop later if needed
     },
 
-    checkWinner: function () {
+    checkWinner: function (player) {
         const row1 = this[0];
         const row2 = this[1];
         const row3 = this[2];
@@ -30,36 +39,56 @@ const board = {
         //check rows
         if (row1[0] != 0 && row1[0] === row1[1] && row1[0] === row1[2]) {
             console.log("winner row 1");
+            this.winner = player;
+            this.gameOver = true;
         }
 
         if (row2[0] != 0 && row2[0] === row2[1] && row2[0] === row2[2]) {
             console.log("winner row 2");
+            this.winner = player;
+            this.gameOver = true;
         }
 
         if (row3[0] != 0 && row3[0] === row3[1] && row3[0] === row3[2]) {
             console.log("winner row 3");
+            this.winner = player;
+            this.gameOver = true;
         }
 
         //check columns
         if (row1[0] != 0 && row1[0] === row2[0] && row1[0] === row3[0]) {
             console.log("winner col 1");
+            this.winner = player;
+            this.gameOver = true;
         }
 
         if (row1[1] != 0 && row1[1] === row2[1] && row1[1] === row3[1]) {
             console.log("winner col 2");
+            this.winner = player;
+            this.gameOver = true;
         }
 
         if (row1[2] != 0 && row1[2] === row2[2] && row1[2] === row3[2]) {
             console.log("winner col 3");
+            this.winner = player;
+            this.gameOver = true;
         }
 
         //check diagonals
         if (row1[0] != 0 && row1[0] === row2[1] && row1[0] === row3[2]) {
             console.log("winner top right diagonal");
+            this.winner = player;
+            this.gameOver = true;
         }
 
         if (row1[2] != 0 && row1[2] === row2[1] && row1[2] === row3[0]) {
             console.log("winner bottom right diagonal");
+            this.winner = player;
+            this.gameOver = true;
+        }
+
+        if (this.tilesPlayed === 9) {
+            this.gameOver = true;
         }
 
     }
---
+++ js/twoplayers.js
@@ -0,0 +1,103 @@
+//this file is for a two player game
+// create a function that toggles between the players
+//let player select a square (get player input)
+// loop through until winner
+let player1turn = true;
+
+$("document").ready(function() {
+    render();
+
+    $("#square00").click(function() {
+        twoPlay(0,0);
+    });
+    $("#square01").click(function() {
+    twoPlay(0,1);
+    });
+    $("#square02").click(function() {
+        twoPlay(0,2);
+    });
+    $("#square10").click(function() {
+        twoPlay(1,0);
+    });
+    $("#square11").click(function() {
+    twoPlay(1,1);
+    });
+    $("#square12").click(function() {
+        twoPlay(1,2);
+    });
+    $("#square20").click(function() {
+        twoPlay(2,0);
+    });
+    $("#square21").click(function() {
+        twoPlay(2,1);
+    });
+    $("#square22").click(function() {
+        twoPlay(2,2);
+    });
+
+    //gameover button
+    $("#playagain").click(function() {
+        board.restart();
+        render();
+        $("#gameover").toggle();
+    });
+
+});
+
+const render = () => {
+    colorSquare(0,0);
+    colorSquare(0,1);
+    colorSquare(0,2);
+    colorSquare(1,0);
+    colorSquare(1,1);
+    colorSquare(1,2);
+    colorSquare(2,0);
+    colorSquare(2,1);
+    colorSquare(2,2);
+
+    if (player1turn) {
+        $("#msg").html("It's Player 1's Turn");
+    }
+    if (!player1turn) {
+        $("#msg").html("It's Player 2's Turn");
+    }
+}
+
+const colorSquare = (row, col) => {
+    if (board[row][col] != 0) {
+        if (board [row][col] === 1) {
+            return $(`#square${row}${col}`).addClass("player1");
+        }
+        return $(`#square${row}${col}`).addClass("player2");
+    }
+    $(`#square${row}${col}`).removeClass("player1");
+    $(`#square${row}${col}`).removeClass("player2");
+}
+
+const twoPlay = (row, col) => {
+    if (board[row][col] != 0) {
+        return;
+    }
+    if (player1turn) {
+        board.select(row,col,1);
+    }
+    else {
+        board.select(row,col,2);
+    }
+
+    if (board.gameOver) {
+        render();
+        if (board.winner === 0) {
+            $("#gameovertext").html("Draw");
+        }
+        else {
+            $("#gameovertext").html(`Player ${board.winner} Wins!`);
+        }
+        $("#gameover").toggle();
+    }
+
+    player1turn = !player1turn;
+    render();
+    
+}
+
